5a148fd9e51908a81eb334292dab6b3f37b81f63
dhananjreddy <dhananjreddy@gmail.com>
2025-09-22 13:22:05 +0000

    command, commands, login

---
+++ golang/commands.go
@@ -0,0 +1,25 @@
+package main
+
+import("errors")
+
+type command struct {
+	Name string
+	Arguments []string
+}
+
+type commands struct {
+	commands map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	f, ok := c.commands[cmd.Name]
+	if !ok {
+		return errors.New("command not found")
+	}
+
+	return f(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.commands[name] = f
+}
--- golang/main.go
+++ golang/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 	"github.com/Dhananjreddy/Bootdev_BlogAggregator/golang/internal/config"
 )
 
 type state struct {
-	cfg *Config
+	config *config.Config
 }
 
 func main()  {
@@ -15,10 +15,25 @@ func main()  {
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	err = cfg.SetUser("Dhananjay")
-	if err != nil {
-		log.Fatalf("error setting username in config: %v", err)
+	
+	newState := &state{
+		config: &cfg,
 	}
-	fmt.Println("DBURL: %s", cfg.DBURL)
-	fmt.Println("CurrentUserName: %s", cfg.CurrentUserName)
+
+	commands := commands{
+		commands: make(map[string]func(*state, command) error),
+	}
+
+	commands.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+	commandName := os.Args[1]
+	commandArgs := os.Args[2:]
+
+	err = commands.run(newState, command{Name: commandName, Arguments: commandArgs})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
